332ada44311d9f4dd2ef694e239cc0cd9aaa4f28
brain <shahriarmehedi95@gmail.com>
2021-11-07 20:46:28 +0600

    function

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 
 //javaScript Array
 
-const friends=['shawon','lelin','anwar'];
-console.log(typeof friends[2]);
+function showMyname(name,age){
+    console.log('my name is '+ name);
+}
+
+showMyname('shahriar');
+showMyname('mehedi');
+
+// let num1=45;
+// let num2=80;
+// let sum=num1+num2;
+// console.log(sum);
+
+function calculateSum(num1,num2){
+    const sum= num1+num2;
+    return sum;
+
+}
+const result = calculateSum(30,40);
+console.log(result);
