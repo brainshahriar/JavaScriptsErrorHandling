728f862fe225aa314101c79220a957f5c546d505
brain <shahriarmehedi95@gmail.com>
2021-11-07 20:37:05 +0600

    updated

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-let name ="shahriar";
-let age=50;
-let hobby ='reading';
-let user={
-    name:'shahriar',
-    age:50,
-    hobby:'sex',
-}
 
-console.log(user);
-console.log(user['hobby']);
+//javaScript Array
+
+const friends=['shawon','lelin','anwar'];
+console.log(typeof friends[2]);
