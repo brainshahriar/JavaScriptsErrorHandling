c20ea116a2dc76ead46a6c04080b208da8355f17
brain <shahriarmehedi95@gmail.com>
2021-11-07 22:04:48 +0600

    loop

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-let apple=10;
-let orange=50;
+// let apple=10;
+// let orange=50;
 
-let temp=apple;
-apple=orange;
-orange=temp;
+// let temp=apple;
+// apple=orange;
+// orange=temp;
 
-console.log(apple,orange);
+// console.log(apple,orange);
+
+
+// const obj={
+//     name:'shahriar',
+//     age:40,
+// }
+// for(let key in obj){
+//     console.log(obj[key]);
+// }
+
