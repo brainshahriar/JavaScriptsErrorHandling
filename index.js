23775a83b2c7fb3982a2e630dfb866fdf703c32e
brain <shahriarmehedi95@gmail.com>
2021-11-07 20:52:53 +0600

    template

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 
 //javaScript Array
 
-function showMyname(name,age){
-    console.log('my name is '+ name);
-}
-
-showMyname('shahriar');
-showMyname('mehedi');
-
-// let num1=45;
-// let num2=80;
-// let sum=num1+num2;
-// console.log(sum);
-
-function calculateSum(num1,num2){
-    const sum= num1+num2;
-    return sum;
-
-}
-const result = calculateSum(30,40);
-console.log(result);
+let name ="boga";
+console.log(`my name is ${name}`);
