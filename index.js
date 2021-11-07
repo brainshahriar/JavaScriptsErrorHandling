b837d4020e7192a9861636afd0f37c471f22932f
brain <shahriarmehedi95@gmail.com>
2021-11-07 20:58:18 +0600

    pesin

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
-//javaScript Array
 
-let name ="boga";
-console.log(`my name is ${name}`);
+
+let person = {
+    name:'shahrar',
+    age:65,
+    isMariied:true,
+    homeAdress:{
+        long:44.56,
+        lat:33.44,
+
+    },
+    friends:['shahriar','mehedi','lelin'],
+};
+console.log(person);
+console.log(person.homeAdress.lat);
