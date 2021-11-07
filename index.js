d34b39e94799120a1b69d57b4dc7125cfe1da560
brain <shahriarmehedi95@gmail.com>
2021-11-07 22:45:50 +0600

    function

--- index.js
+++ index.js
@@ -16,3 +16,34 @@
 //     console.log(obj[key]);
 // }
 
+// for(let shahriar=1; shahriar<=3; shahriar++){
+//     for(let babu=1;babu<=3;babu++){
+//         console.log(babu);
+//     }
+   
+// }
+
+// let message="shahriar mehedi";
+// console.log(message.toUpperCase());
+
+//function
+
+function aboutMe(name,age){
+    const message =`i am ${name} and i am ${age} years old`;
+    console.log(message);
+}
+aboutMe('sha',55);
+aboutMe('ba',60);
+
+// let number1=20;
+// let number2=30;
+
+function calSum(number1,number2){
+    let sum=number1+number2;
+    return sum;
+}
+let result=calSum(10,21);
+
+
+console.log(result);
+
