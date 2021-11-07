4a85297189460153ca2619750d5a4cca1cac74f8
brain <shahriarmehedi95@gmail.com>
2021-11-07 21:02:42 +0600

    operator

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-
-
-
-let person = {
-    name:'shahrar',
-    age:65,
-    isMariied:true,
-    homeAdress:{
-        long:44.56,
-        lat:33.44,
-
-    },
-    friends:['shahriar','mehedi','lelin'],
-};
-console.log(person);
-console.log(person.homeAdress.lat);
+let price=200;
+console.log(price===200);
